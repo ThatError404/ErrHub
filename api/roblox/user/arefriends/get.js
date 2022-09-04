a6fdbcff57cd1aa6a3a2002a4c9c45c365c4cbd3
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-09-04 02:46:37 -0500

    Update get.js

--- api/roblox/user/arefriends/get.js
+++ api/roblox/user/arefriends/get.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch');
 
 export default (req, res) => {
-    const { id } = req.query.id;
-    const { fid } = req.query.fid;
+    const { id } = req.query;
+    const { fid } = req.query;
     if (!id) {
         res.setHeader("Content-Type", 'application/json');
         res.send(JSON.stringify({
