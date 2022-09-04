1abaf759f3ab2cfb0eef461e24645a81978ca753
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-09-04 10:42:58 -0500

    Add files via upload

--- api/roblox/user/friends/index.js
+++ api/roblox/user/friends/index.js
@@ -2,7 +2,7 @@ export default (req, res) => {
     res.setHeader("Content-Type", 'application/json');
     res.send(JSON.stringify({
         types: [{
-            "Get User's Friends": "/roblox/user/friends/get?id={id}%sort={sort order}",
+            "Get User's Friends": "/roblox/user/friends/get?id={id}&sort={sort order}",
         }]
     }, null, 4))
 }
