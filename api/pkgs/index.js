abd7dc049eb5965125e86bca018ec06fa76fa2f4
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-09-04 22:05:10 -0500

    Delete api directory

--- api/pkgs/index.js
+++
@@ -1,7 +0,0 @@
-export default (req, res) => {
-    res.json({
-        error: 'ERR 69 No package argument provided',
-        message: 'Please provide a package argument in the URL using the format /api/pkgs/get?pkg=package-name',
-        status: 400
-    })
-}
--- api/roblox/user/followers/index.js
+++
@@ -1,8 +0,0 @@
-export default (req, res) => {
-    res.setHeader("Content-Type", 'application/json');
-    res.send(JSON.stringify({
-        types: [{
-            "Get User's Followers": "/roblox/user/followers/get?id={id}",
-        }]
-    }, null, 4))
-}
