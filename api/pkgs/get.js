207de70b82a787d643b7d884dea0c14cd3fb5b51
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-09-04 21:54:31 -0500

    Update get.js

--- api/pkgs/get.js
+++ api/pkgs/get.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 export default (req, res) => {
     let raw = req.query.pkg;
     let pkg = raw.toLowerCase();
-    if (fs.existsSync(`public/${pkg}.json.err`)) {
-        res.status(200).json(JSON.parse(fs.readFileSync(`./public/${pkg}.json.err`)));
+    if (fs.existsSync(`public/${pkg}.json`)) {
+        res.status(200).json(JSON.parse(fs.readFileSync(`./public/${pkg}.json`)));
     } else {
         // Get files with a similar name
         res.status(404).json({
