7b628cf333e35e2a9688020d5a34753172f484dd
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-09-04 21:41:05 -0500

    Update get.js

--- api/pkgs/get.js
+++ api/pkgs/get.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 export default (req, res) => {
     const { name } = req.query;
+    console.log(name)
     if (fs.existsSync(`./public/${name}.json.err`)) {
         res.status(200).json(JSON.parse(fs.readFileSync(`./public/${name}.json.err`)));
     } else {
