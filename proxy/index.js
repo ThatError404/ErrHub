1bb8e367c07fcbff45541248d5502d03bdf88d13
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-09-12 17:26:13 -0500

    Add files via upload

--- proxy/index.js
+++ proxy/index.js
@@ -2,12 +2,16 @@ const fetch = require('node-fetch');
 
 export default (req, res) => {
     const { url } = req.query;
-    fetch(url)
-        .then(response => response.text())
-        if (response.text().match(/src="http/)) {
-            const httpValue = response.text().match(/src="http/);
-            const httpsValue = httpValue.replace('http', 'https://errhub.dev/proxy?url=http');
-            response.text().replace(httpValue, httpsValue);
+    if (!url) {
+        return res.status(400).send('Missing URL parameter');
+    } else {
+        fetch(url)
+            .then(response => response.text())
+            if (response.text().match(/src="http/)) {
+                const httpValue = response.text().match(/src="http/);
+                const httpsValue = httpValue.replace('http', 'https://errhub.dev/proxy?url=http');
+                response.text().replace(httpValue, httpsValue);
+            }
+            res.send(response.text());
         }
-        res.send(response.text());
     }
