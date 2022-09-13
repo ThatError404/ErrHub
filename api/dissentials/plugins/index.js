7ef40abe525c27e058c4645f85af5f59d4d21f46
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-09-13 14:53:58 -0500

    Update index.js

--- api/dissentials/plugins/index.js
+++ api/dissentials/plugins/index.js
@@ -3,9 +3,9 @@ export default (req, res) => {
     const { type } = req.query;
     if (type === 'amount') {
         const fs = require('fs');
-        const plugins = fs.readdirSync('../plugins');
+        const plugins = fs.readdirSync('.');
         const folders = plugins.filter((plugin) => {
-            return fs.statSync(`../plugins/${plugin}`).isDirectory();
+            return fs.statSync(`./${plugin}`).isDirectory();
         });
         res.send(JSON.stringify({
             amount: folders.length
