90a852d7984f22b38859bfdcd6ad83b130c63f79
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-09-17 20:29:23 -0500

    Delete api/roblox directory

--- api/roblox/user/index.js
+++
@@ -1,9 +0,0 @@
-export default (req, res) => {
-    res.setHeader("Content-Type", 'application/json');
-    res.send(JSON.stringify({
-        types: [{
-            "Get User Status (By ID)": "/roblox/user/status?type=id&id={id}",
-            "Get User Status (By Username)": "/roblox/user/status?type=username&username={username}"
-        }]
-    }, null, 4))
-}
