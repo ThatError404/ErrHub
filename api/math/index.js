914e1ae5ba37fe6e7e3c3a4c13d2c79f88f8889e
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-09-17 20:43:48 -0500

    Update index.js

--- api/math/index.js
+++ api/math/index.js
@@ -1,6 +1,6 @@
 export default function (req, res) {
-    const { data } = req.body;
-    if (data.idle === true) {
+    const data = req.body;
+    if (data.idle == "true" || data.idle == true) {
         let work = true
         res.status(200).json({
             work,
